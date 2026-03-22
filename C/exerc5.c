6e6b07002680f7c66062d4f972fd5d329ec3cbf5
Isaki23 <isaquefonseca230706@gmail.com>
2026-03-22 17:54:20 -0300

    Exercício 5.
    //5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse 
    //usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).

---
+++ C/exerc5.c
@@ -0,0 +1,25 @@
+//exerc 5  
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse 
+//usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).
+
+
+
+
+
+int main()
+{
+	float sm,su,st;
+	printf("Salário:\n");
+	scanf("%f", &su);
+    sm = 1293.20;
+	
+	st = su / sm;
+	printf("Você recebe %1.f Salário(s) minímos.", st);
+	
+	return 0;
+}
+
