dbdbc9e855b027d69a0cc086acd76e46f7269a4d
Isaki23 <isaquefonseca230706@gmail.com>
2026-04-07 14:59:40 -0300

    Multiprocessing - python
    Teste do pacote multiprocessing em python.

---
+++ multithreading-python/first.py
@@ -0,0 +1,33 @@
+import multiprocessing
+
+def main():
+    
+    print("a")
+    return 0
+if __name__=="__main__":
+ main()
+
+ def abacaxi():
+    c = 10
+    a = int(input())
+    soma = int()
+    soma = c + a
+    print(f'{soma}')
+    return 0
+   
+ if __name__=="__main__":
+    abacaxi()
+
+
+
+def pera():
+   d = 20
+   na = int(input())
+   soma1 = int()
+   soma1 = d + na
+   print(f'{soma1}')
+   return 0
+
+
+if __name__=="__main__":
+   pera()
